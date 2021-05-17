79d3f54d41972de65d700f34b227e1dbfe107dc8
odedron100 <odedron84@gmail.com>
2021-05-17 16:18:45 +0300

    Add design to cards

--- src/cmps/NewListButton/NewListButton.jsx
+++ src/cmps/NewListButton/NewListButton.jsx
@@ -8,71 +8,19 @@ import './NewListButton.scss'
 
 export const NewListButton = (props) => {
 
-    // const [isListFormOpen, setIsListFormOpen] = useState(false);
-    const formContainerRef = useRef(null);
-    const openFormButtonRef = useRef(null);
-    const lists = useSelector(state => state.listReducer.lists)
-    // const [activeList,setActiveList] = useState(listService.getEmptyList())
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(loadLists())
     }, []);
 
-    //   const handleDocumentClick = useCallback((e) => {
-    //     if (e.target === openFormButtonRef.current) {
-    //         return;
-    //     }
-
-    //     console.log('isListFormOpen', isListFormOpen);
-    //     if (!formContainerRef.current || !isListFormOpen) {
-    //         return;
-    //     }
-
-    //     if (!formContainerRef.current.contains(e.target)) {
-    //         console.log('closing!');
-    //         setIsListFormOpen(false);
-    //     }
-    // }, [formContainerRef, isListFormOpen, openFormButtonRef]);
-
-    // useEffect(() => {
-    //     document.addEventListener('click', handleDocumentClick);
-    //     return () => {
-    //         document.removeEventListener('click', handleDocumentClick);
-    //     }
-    // });
-
-    // const updateListTitle = (e) =>{
-    //     setNewListInputValue(e.target.value);
-    //     setActiveList({...activeList, title: e.target.value});
-    // }
-
-    // const toggleListForm = () => {
-    //     console.log('current state:', isListFormOpen);
-    //     console.log('going to change to:', !isListFormOpen);
-    //     setIsListFormOpen(!isListFormOpen);
-    // }
-
     const onAddNewList = (newList) => {
         dispatch(saveList({...listService.getEmptyList(), ...newList }))
-        // setActiveList(listService.getEmptyList())
-        // setNewListInputValue('');
-        // e.target.value = '';
     }
 
     return (
         <section className="new-list-button-container">
             <FormButton itemType="list" onSubmit={onAddNewList} />
-            {/* {isListFormOpen ?
-                <section className="new-list-form" ref={formContainerRef}>
-                    <input type="text" placeholder="Enter list title..." onChange={updateListTitle} value={newListInputValue}/>
-                    <footer>
-                        <button className="add-list trello-button" onClick={onAddNewList}> Add List</button>
-                        <button className="remove-list" onClick={toggleListForm}><i className="fas fa-times"></i></button>
-                    </footer>
-                </section>
-            :
-            <button ref={openFormButtonRef} className="create-list-button" onClick={toggleListForm}>+ Add a list</button>} */}
         </section>
     )
 }
